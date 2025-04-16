921a680b0bab1290d9e0d24b6a66c9112eca9922
NoahGallo <gallo.noah@gmail.com>
2025-04-16 16:13:53 +0200

    Working on complete docker-compose file. dockerized my api. dockerized frontend

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,27 +1,26 @@
 export const products = [
-    {
-      id: 1,
-      name: "iPhone 14 Pro",
-      price: 999,
-      image: "https://via.placeholder.com/200x200?text=iPhone+14+Pro",
-    },
-    {
-      id: 2,
-      name: "MacBook Pro 16\"",
-      price: 2499,
-      image: "https://via.placeholder.com/200x200?text=MacBook+Pro+16",
-    },
-    {
-      id: 3,
-      name: "AirPods Pro",
-      price: 249,
-      image: "https://via.placeholder.com/200x200?text=AirPods+Pro",
-    },
-    {
-      id: 4,
-      name: "iPad Pro",
-      price: 1099,
-      image: "https://via.placeholder.com/200x200?text=iPad+Pro",
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "iPhone 14 Pro",
+    price: 999,
+    image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-14-pro-spaceblack-202404?wid=2000&hei=1897&fmt=jpeg&qlt=95&.v=1712956909430",
+  },
+  {
+    id: 2,
+    name: "MacBook Pro 16\"",
+    price: 2499,
+    image: "https://www.bhphotovideo.com/cdn-cgi/image/fit=scale-down,width=500,quality=95/https://www.bhphotovideo.com/images/images500x500/apple_z16_mbp_194_16_macbook_pro_m4_1730367032_1859777.jpg"
+  },
+  {
+    id: 3,
+    name: "AirPods Pro",
+    price: 249,
+    image: "https://media.hifi.lu/sys-master/products/9459682902046/1440x1440.B1196927_01.webp",
+  },
+  {
+    id: 4,
+    name: "iPad Pro",
+    price: 1099,
+    image: "https://lineheart.lu/cdn/shop/files/86c200464f7d11edb8d8feedce56ad7063977AppleMNXQ3NF_A.png?v=1739197698&width=1946",
+  },
+];
